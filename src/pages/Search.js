d6cd5e677653c51df17ae708439c12d11f01256b
Stephen Oh <oh.stephen98@gmail.com>
2021-03-13 16:36:58 -0800

    added API functionality

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,9 +1,65 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { Button, Container, Row, Col, InputGroup, FormControl } from 'react-bootstrap';
-
 import SearchResults from '../components/SearchResults';
+import fetch from 'isomorphic-unfetch';
+import axios from 'axios';
+const APIkey = 'RX0RyDW9JYbHkUrMKp3REkF51-YsQbZbSagBgXZ4HgpZn2WMPBwXat-LzkxiRHZkCEKbue5Yd2qarbhpxm_Ib3DOpF9dIIaLwc5-I2YQs8V4de5ATm8YJbJaQwtsXnYx'
+
+function useSearch(query){
+  const [repos, setRepos] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [isDone, setIsDone] = useState(false);
+  const url = `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=${query}&categories=bubbletea`;
+  useEffect(() => {
+    let ignore = false;
+    const controller = new AbortController();
+    async function fetchSearchResults() {
+      let responseBody = {};
+      setIsLoading(true);
+      setIsError(false);
+      setIsDone(false);
+
+      try{
+        const res = await fetch(url,
+        { headers: {
+            "Authorization": `Bearer ${APIkey}`,
+            "Access-Control-Allow-Origin":"*"
+            },
+          signal: controller.signal, }
+        );
+        responseBody = await res.json();
+      } catch (e) {
+        if (e instanceof DOMException) {
+          console.log("HTTP request aborted");
+        } else{
+          console.log("We got an error:", e);
+          setIsError(true);
+        }
+      }
+
+      if (!ignore){
+        console.log('RESPONSEBODY==', responseBody);
+        setRepos(responseBody || []);
+        setIsLoading(false);
+        setIsDone(true);
+      }
+    }
+    if (query){
+      fetchSearchResults();
+    }
+    return () => {
+      controller.abort();
+      ignore = true;
+    }
+  }, [ query ]);
+  return [repos,isLoading,isError, isDone];
+}
 
 function Search() {
+  const [data, isLoading, isError, isDone] = useSearch('Corvallis');
+  const isQuerySafe = (!isLoading && !isError && isDone);
+  console.log("data is ==", data);
   return (
     <div>
       <Container>
