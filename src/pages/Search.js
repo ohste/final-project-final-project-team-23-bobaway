9cb39fc59cf8d234d0e1874837b09818d99bcf1b
Aeijan <bajracae@oregonstate.edu>
2021-03-13 16:07:12 -0800

    changed some names, can get text from input box in search

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import { Button, Container, Row, Col, InputGroup, FormControl } from 'react-bootstrap';
+import React, {useState} from 'react';
+import { Button, Container, Form, Row, Col, InputGroup, FormControl } from 'react-bootstrap';
 
 import SearchResults from '../components/SearchResults';
 
 function Search() {
+  const [input, setInput] = useState("");
+
   return (
     <div>
       <Container>
@@ -15,16 +17,27 @@ function Search() {
         <Row className="text-center">
           <Col md={2}></Col>
           <Col md={8} className="my-auto">
-            <InputGroup className="mb-3">
-              <FormControl
-                placeholder="City, State, Country"
-                aria-label="Default"
-                aria-describedby="inputGroup-sizing-default"
-              />
-              <InputGroup.Append>
-                <Button variant="primary">Search</Button>
-              </InputGroup.Append>
-            </InputGroup>
+            <Form
+              onSubmit={(e) => {
+                e.preventDefault();
+                console.log(input)
+              }}
+            >
+              <InputGroup className="mb-3">
+                <FormControl
+                  placeholder="City, State, Country"
+                  aria-label="Default"
+                  aria-describedby="inputGroup-sizing-default"
+                  value={input}
+                  onChange={(e) => setInput(e.target.value)}
+                />
+                <InputGroup.Append>
+                  <Button variant="primary" type="submit">
+                    Search
+                  </Button>
+                </InputGroup.Append>
+              </InputGroup>
+            </Form>
           </Col>
           <Col md={2}></Col>
         </Row>
