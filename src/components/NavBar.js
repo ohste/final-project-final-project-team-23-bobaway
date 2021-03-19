fa9d2e63d51be2bfa59f89f88e7c57f0f74dcf53
Aeijan <bajracae@oregonstate.edu>
2021-03-18 23:35:08 -0700

    Commented out Shop Information from Navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,12 +8,12 @@ function NavBar() {
         <Navbar.Brand href="/"><span style={{color: "blanchedalmond", fontFamily: "Pacifico"}}>BobaWay</span></Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav>
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/search">Search</Nav.Link>
-            <Nav.Link href="/about">About</Nav.Link>
-            <Nav.Link href="/contact">Contact</Nav.Link>
-            <Nav.Link href="/shopInfo">Shop Information</Nav.Link>
+          <Nav >
+            <Nav.Link href="/" style={{color: "blanchedalmond"}}>Home</Nav.Link>
+            <Nav.Link href="/search" style={{color: "blanchedalmond"}}>Search</Nav.Link>
+            <Nav.Link href="/about" style={{color: "blanchedalmond"}}>About</Nav.Link>
+            <Nav.Link href="/contact" style={{color: "blanchedalmond"}}>Contact</Nav.Link>
+            {/* <Nav.Link href="/shopInfo">Shop Information</Nav.Link> */}
           </Nav>
         </Navbar.Collapse>
       </Navbar>
