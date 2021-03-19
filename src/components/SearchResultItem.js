8dc3817dacd142b210d971eaba1c8a8490629862
Aeijan <bajracae@oregonstate.edu>
2021-03-18 19:52:39 -0700

    Rating system in place, ready for redux

--- src/components/SearchResultItem.js
+++ src/components/SearchResultItem.js
@@ -1,32 +1,35 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Card } from 'react-bootstrap';
 
 function SearchResultItem(props) {
   const {shopInfo} = props;
   return (
     <div>
-      <Card className="mb-3" style={{ width: "19rem" }}>
-        <Card.Img variant="top" src={shopInfo.image_url} key={shopInfo.id} />
-        <Card.Body>
-          <Card.Title>Shop Name: {shopInfo.name}</Card.Title>
-          <Card.Text>Phone Number: {shopInfo.display_phone}</Card.Text>
-          <Card.Text>
-            Address:{" "}
-            {shopInfo.location.display_address.map((text) => (
-              <span> {text}</span>
-            ))}
-          </Card.Text>
-          <Card.Text>
-            Closed: {shopInfo.is_closed ? "Closed" : "Open"}
-          </Card.Text>
-          <Card.Text>
-            Transactions:{" "}
-            {shopInfo.transactions.map((text) => (
-              <span>{text} | </span>
-            ))}
-          </Card.Text>
-        </Card.Body>
-      </Card>
+      <Link to={`/shops/${shopInfo.id}`}>
+        <Card className="mb-3" style={{ width: "19rem" }}>
+          <Card.Img variant="top" src={shopInfo.image_url} key={shopInfo.id} />
+          <Card.Body>
+            <Card.Title>{shopInfo.name}</Card.Title>
+            <Card.Text>{shopInfo.display_phone}</Card.Text>
+            <Card.Text>
+              Address:{" "}
+              {shopInfo.location.display_address.map((text) => (
+                <span> {text}</span>
+              ))}
+            </Card.Text>
+            <Card.Text>
+              Status: {shopInfo.is_closed ? "Closed" : "Open"}
+            </Card.Text>
+            <Card.Text>
+              Transactions:{" "}
+              {shopInfo.transactions.map((text) => (
+                <span>{text} | </span>
+              ))}
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </Link>
     </div>
   );
 }
--- src/pages/ShopInfo.js
+++ src/pages/ShopInfo.js
@@ -3,17 +3,36 @@ import { Button, Card, Col, Container, Row } from 'react-bootstrap';
 
 import BeautyStars from 'beauty-stars';
 
+import RateBox from '../components/RateBox';
+
 import "./ShopInfo.css";
 
 // https://reactjsexample.com/a-simple-and-beauty-star-rating-for-react/
 
 function ShopInfo() {
   const [stars, setStars] = useState(0);
+  const [showRateBox, setShowRateBox] = useState(false);
+  const [raterName, setRaterName] = useState("");
+  const [raterText, setRaterText] = useState("");
+  const [raterStarValue, setRaterStarValue] = useState(0);
   
   // need this for hardcoding
-  useEffect(() => {
-    setStars(2);
-  })
+  // useEffect(() => {
+  //   setStars(2);
+  // })
+
+  const handleClose = () => setShowRateBox(false);
+  const handleShow = () => setShowRateBox(true);
+
+  const handleAddRatingBtn = () => {
+    console.log("added:", showRateBox);
+    setShowRateBox(!showRateBox);
+  }
+
+  const handleSubmitRatingBtn = () => {
+    console.log(raterName, raterText, raterStarValue);
+    console.log("submit button clicked");
+  }
 
   return (
     <Container>
@@ -22,11 +41,13 @@ function ShopInfo() {
           <h1>Shop Name</h1>
         </Col>
       </Row>
+
       <Row>
         <Col md={12} className="text-center mb-3">
           <BeautyStars value={stars} />
         </Col>
       </Row>
+
       <Row>
         <Col md={2}></Col>
         <Col md={8} className="mb-3">
@@ -36,21 +57,34 @@ function ShopInfo() {
               <Card.Text>Address: </Card.Text>
               <Card.Text>Hour: </Card.Text>
               <Card.Text>Description: </Card.Text>
+              <Button onClick={handleAddRatingBtn}>Add Rating</Button>
             </Card.Body>
           </Card>
         </Col>
         <Col md={2}></Col>
       </Row>
+
+      <Row>
+        <Col md={2}></Col>
+        <Col md={8} className="mb-3">
+          {showRateBox && <RateBox 
+                            submitHandle={handleSubmitRatingBtn} 
+                            setName={setRaterName}
+                            setText={setRaterText}
+                            setStarValue={setRaterStarValue}
+                          />}
+        </Col>
+        <Col md={2}></Col>
+      </Row>
+
       <Row>
         <Col md={2}></Col>
         <Col md={8} className="mb-3">
           <Card>
-            <Card.Header>Ratings</Card.Header>
             <Card.Body>
               <Card.Text>Address: </Card.Text>
               <Card.Text>Hour: </Card.Text>
               <Card.Text>Description: </Card.Text>
-              <Button>Add Rating</Button>
             </Card.Body>
           </Card>
         </Col>
